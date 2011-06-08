55c32a942e92eed5f17ca3e4faf9108af54c0202
Brendan Abbott <brendan@randb.com.au>
2011-06-08 23:14:26 +1000

    Update SecurePay extension to add Preferences panel

--- extension.driver.php
+++ extension.driver.php
@@ -1,26 +1,29 @@
 <?php
-	
+
 	class Extension_SecurePay extends Extension {
-		
+
+		const GATEWAY_ERROR = 100;
+		const DATA_ERROR = 200;
+
 		public function about() {
 			return array(
 				'name'			=> 'SecurePay',
 				'version'		=> '1.0',
 				'release-date'	=> '2009-11-03',
 				'author'		=> array(
-					'name'			=> '',
-					'website'		=> '',
-					'email'			=> ''
+					array(
+						'name' => 'Brendan Abbott',
+						'email' => 'brendan@bloodbone.ws'
+					),
+					array(
+						'name' => 'Henry Singleton'
+					)
 				),
 				'description' => 'Manage and track payments using SecurePay.'
 			);
 		}
-		
-		/*//////////////////////////////////////////////////////////////
-		//	Setup
-		/////////////////////////////////////////////////////////////*/
-		
-		private $defaults = array(
+
+		private static $defaults = array(
 			'messageID' => '',
 			'messageTimestamp' => '',
 			'timeoutValue' => '60',
@@ -29,15 +32,15 @@
 			'password' => '',
 			'RequestType' => 'Payment',
 			'txnType' => '0',
-			'txnSource' => '23',
+			'txnSource' => '0',
 			'amount' => '',
 			'purchaseOrderNo' => '',
 			'cardNumber' => '',
 			'expiryDate' => '',
 			'cvv' => ''
 		);
-		
-		private $required_fields = array(
+
+		private static $required_fields = array(
 			'messageID',
 			'messageTimestamp',
 			'timeoutValue',
@@ -52,8 +55,8 @@
 			'cardNumber',
 			'expiryDate',
 		);
-		
-		private $approved_codes = array(
+
+		private static $approved_codes = array(
 			'00', //Approved
 			'08', //Honour with ID
 			'10', //Partial Amount Approved
@@ -61,198 +64,293 @@
 			'16', //Approved, Update Track 3
 			'77'  //Approved (ANZ only)
 		);
-		
+
+	/*-------------------------------------------------------------------------
+		Definition:
+	-------------------------------------------------------------------------*/
+
 		public function install() {
-			$this->_Parent->Configuration->set('merchant-id', '', 'securepay');
-			$this->_Parent->Configuration->set('merchant-password', '', 'securepay');
-			$this->_Parent->Configuration->set('production-gateway-uri', 'https://www.securepay.com.au/xmlapi/payment', 'securepay');
-			$this->_Parent->Configuration->set('development-gateway-uri', 'https://www.securepay.com.au/test/payment', 'securepay');
-			$this->_Parent->Configuration->set('development-mode', false, 'securepay');
-			$this->_Parent->saveConfig();
-		}
-		
-		public function uninstall() {
-			$this->_Parent->Configuration->remove('securepay');
-			$this->_Parent->saveConfig();
-		}
-		
-		/*//////////////////////////////////////////////////////////////
-		//	Utilities
-		/////////////////////////////////////////////////////////////*/
-		
-		public function configuration() {
-			return $this->_Parent->Configuration;
-		}
-		
-		public function isTesting() {
-			return (boolean)$this->configuration()->get('development-mode', 'securepay');
-		}
-		
-		public function getgatewayURI() {
-			$mode = 'production';
-			if ($this->isTesting()) $mode = 'development';
-			return (string)$this->configuration()->get("{$mode}-gateway-uri", 'securepay');
-		}
-		
-		public function getCustomerId() {
-			return (string)$this->configuration()->get("merchant-id", 'securepay');
+			Symphony::Configuration()->set('merchant-id', '', 'securepay');
+			Symphony::Configuration()->set('merchant-password', '', 'securepay');
+			Symphony::Configuration()->set('production-gateway-uri', 'https://www.securepay.com.au/xmlapi/payment', 'securepay');
+			Symphony::Configuration()->set('development-gateway-uri', 'https://www.securepay.com.au/test/payment', 'securepay');
+			Symphony::Configuration()->set('gateway-mode', 'development', 'securepay');
+			Administration::instance()->saveConfig();
 		}
 
-		public function getCustomerPassword() {
-			return (string)$this->configuration()->get("merchant-password", 'securepay');
+		public function uninstall() {
+			Symphony::Configuration()->remove('securepay');
+			Administration::instance()->saveConfig();
 		}
-	
-		public function getFields($section_id) {
-			$frontend = Frontend::instance();
-			$sectionManager = new SectionManager($frontend);
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'appendPreferences'
+				),
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'Save',
+					'callback'	=> 'savePreferences'
+				)
+			);
+		}
+
+	/*-------------------------------------------------------------------------
+		Utilities:
+	-------------------------------------------------------------------------*/
+
+		public static function isTesting() {
+			return Symphony::Configuration()->get('gateway-mode', 'securepay') == 'development';
+		}
+
+		public static function getGatewayURI() {
+			$mode = self::isTesting() ? 'development' : 'production';
+			return (string)Symphony::Configuration()->get("{$mode}-gateway-uri", 'securepay');
+		}
+
+		public static function getMerchantId() {
+			return (string)Symphony::Configuration()->get("merchant-id", 'securepay');
+		}
+
+		public static function getMerchantPassword() {
+			return (string)Symphony::Configuration()->get("merchant-password", 'securepay');
+		}
+
+		public static function getFields($section_id) {
+			$sectionManager = new SectionManager(Frontend::instance());
 			$section = $sectionManager->fetch($section_id);
-			
-			if (empty($section)) return null;
-			
+
+			if (!($section instanceof Section)) return array();
+
 			return $section->fetchFields();
 		}
-	
-		public function fetchEntry($entries) {
-			$frontend = Frontend::instance();
-			$entryManager = new EntryManager($frontend);
+
+		public static function fetchEntries($entries) {
+			$result = array();
+
+			if (!is_array($entries)) $entries = array($entries);
+
+			foreach ($entries as $entry) {
+				$result[$entry] = self::fetchEntry($entry);
+			}
+
+			return $result;
+		}
+
+		public static function fetchEntry($entries) {
+			$entryManager = new EntryManager(Frontend::instance());
 			$entries = $entryManager->fetch($entries);
 
-			$finalentries  = array();
 			foreach ($entries as $order => $entry) {
-				$section_id = $entryManager->fetchEntrySectionID($entry->_fields['id']);
+				$section_id = $entry->get('section_id');
 
-				$fields = $this->getFields($section_id);
-				$entry_data = array('id'=>$entry->_fields['id']);
-				
+				$fields = self::getFields($section_id);
+				$entry_data = array(
+					'id'=> $entry->get('id')
+				);
+
 				foreach ($fields as $field) {
-					$field_handle = Lang::createHandle($field->get('label'));
-					$entry_data[$field_handle] = $entry->getData($field->get('id'));
+					$entry_data[$field->get('element_name')] = $entry->getData($field->get('id'));
 				}
-				//$finalentries[$entry->_fields['id']] = $entry_data;
+
 				return $entry_data;
 			}
-			return $finalentries;
-		}
-		
-		public function fetchEntries($entries) {
-			if (!is_array($entries)) $entries = array($entries);
-			$returnarray = array();
-			foreach ($entries as $entry) {
-				$returnarray[$entry] = $this->fetchEntry($entry);
-			}
-			return $returnarray;
 		}
 
-		/*//////////////////////////////////////////////////////////////
-		//	Process Payments
-		/////////////////////////////////////////////////////////////*/
-		
-		public function process_transaction($values) {
-			//Merge Defaults and passed values
-			$request_array = array_merge($this->defaults,$values);
-			
-			//Authentication
-			$request_array['merchantID'] = $this->getCustomerId();
-			$request_array['password'] = $this->getCustomerPassword();
-			
-			//Message Info
+	/*-------------------------------------------------------------------------
+		Delegate Callbacks:
+	-------------------------------------------------------------------------*/
+
+		/**
+		 * Allows a user to enter their eWay details to be saved into the Configuration
+		 *
+		 * @uses AddCustomPreferenceFieldsets
+		 */
+		public static function appendPreferences($context) {
+			// If the Payment Gateway Interface extension is installed, don't
+			// double display the preference, unless this function is called from
+			// the `pgi-loader` context.
+			if(
+				Symphony::ExtensionManager()->fetchStatus('pgi_loader') == EXTENSION_ENABLED
+				xor isset($context['pgi-loader'])
+			) return;
+
+			$fieldset = new XMLElement('fieldset');
+			$fieldset->setAttribute('class', 'settings');
+			$fieldset->appendChild(new XMLElement('legend', __('SecurePay')));
+
+			$div = new XMLElement('div', null, array('class' => 'group'));
+
+			// Merchant ID
+			$label = new XMLElement('label', __('Merchant ID'));
+			$label->appendChild(
+				Widget::Input('settings[securepay][merchant-id]', self::getMerchantId())
+			);
+			$div->appendChild($label);
+
+			// Merchant ID
+			$label = new XMLElement('label', __('Merchant Password'));
+			$label->appendChild(
+				Widget::Input('settings[securepay][merchant-password]', self::getMerchantPassword())
+			);
+			$div->appendChild($label);
+			$fieldset->appendChild($div);
+
+			$div = new XMLElement('div');
+
+			// Build the Gateway Mode
+			$label = new XMLElement('label', __('Gateway Mode'));
+			$options = array(
+				array('development', self::isTesting(), __('Development')),
+				array('production', !self::isTesting(), __('Production'))
+			);
+
+			$label->appendChild(Widget::Select('settings[securepay][gateway-mode]', $options));
+			$div->appendChild($label);
+
+			$fieldset->appendChild($div);
+			$context['wrapper']->appendChild($fieldset);
+		}
+
+		/**
+		 * Saves the Customer ID and the gateway mode to the configuration
+		 *
+		 * @uses savePreferences
+		 */
+		public function savePreferences(array &$context){
+			$settings = $context['settings'];
+
+			// Active Section
+			Symphony::Configuration()->set('merchant-id', $settings['securepay']['merchant-id'], 'securepay');
+			Symphony::Configuration()->set('merchant-password', $settings['securepay']['merchant-password'], 'securepay');
+			Symphony::Configuration()->set('gateway-mode', $settings['securepay']['gateway-mode'], 'securepay');
+
+			Administration::instance()->saveConfig();
+		}
+
+	/*-------------------------------------------------------------------------
+		Process Transaction:
+	-------------------------------------------------------------------------*/
+
+		public static function processTransaction(array $values = array()) {
+			// Merge Defaults and passed values
+			$request_array = array_merge(Extension_SecurePay::$defaults, $values);
+
+			// Authentication
+			$request_array['merchantID'] = self::getMerchantId();
+			$request_array['password'] = self::getMerchantPassword();
+
+			// Message Info
 			$request_array['messageID'] = md5(time());
 			$request_array['messageTimestamp'] = time();
-			
-			//Check for missing fields
+
+			$request_array = array_intersect_key($request_array, Extension_SecurePay::$defaults);
+
+			// Check for missing fields
 			$valid_data = true;
 			$missing_fields = array();
-			foreach ($this->required_fields as $field_name) {
-				if ($request_array[$field_name] == '' OR !array_key_exists($field_name,$request_array)) {
+			$error = null;
+			foreach (Extension_SecurePay::$required_fields as $field_name) {
+				if (!array_key_exists($field_name, $request_array) || empty($request_array[$field_name])) {
 					$missing_fields[] = $field_name;
 					$valid_data = false;
 				}
 			}
-			
-			if ($valid_data) {
 
-				//Generate the XML
-				$SecurePayMessage = new XMLElement('SecurePayMessage');
+			// The data is invalid, return a `DATA_ERROR`
+			if(!$valid_data) {
+				return(array(
+					'status' => __('Data error'),
+					'response-code' => Extension_SecurePay::DATA_ERROR,
+					'response-message' => __('Missing Fields: %s', array(implode(', ', $missing_fields))),
+					'missing-fields' => $missing_fields
+				));
+			}
 
-				$MessageInfo = new XMLElement('MessageInfo');
-				$MerchantInfo = new XMLElement('MerchantInfo');
-				$RequestType = new XMLElement('RequestType',$request_array['RequestType']);
-				$Payment = new XMLElement('Payment');
-				$TxnList = new XMLElement('TxnList');
-				$Txn = new XMLElement('Txn');
-				$CreditCardInfo = new XMLElement('CreditCardInfo');
-				
-				$MessageInfo->appendChild(new XMLElement('messageID',$request_array['messageID']));
-				$MessageInfo->appendChild(new XMLElement('messageTimestamp',$request_array['messageTimestamp']));
-				$MessageInfo->appendChild(new XMLElement('timeoutValue',$request_array['timeoutValue']));
-				$MessageInfo->appendChild(new XMLElement('apiVersion',$request_array['apiVersion']));
-				
-				$MerchantInfo->appendChild(new XMLElement('merchantID',$request_array['merchantID']));
-				$MerchantInfo->appendChild(new XMLElement('password',$request_array['password']));
-				
-				$Txn->setAttribute('ID', '1');
-				$Txn->appendChild(new XMLElement('txnType',$request_array['txnType']));
-				$Txn->appendChild(new XMLElement('txnSource',$request_array['txnSource']));
-				$Txn->appendChild(new XMLElement('amount',$request_array['amount']));
-				$Txn->appendChild(new XMLElement('purchaseOrderNo',$request_array['purchaseOrderNo']));
-				
-				$CreditCardInfo->appendChild(new XMLElement('cardNumber',$request_array['cardNumber']));
-				$CreditCardInfo->appendChild(new XMLElement('expiryDate',$request_array['expiryDate']));
-				$CreditCardInfo->appendChild(new XMLElement('cvv', $request_array['cvv']));
-				
-				$Txn->appendChild($CreditCardInfo);
-				
-				$TxnList->setAttribute('count','1');
-				$TxnList->appendChild($Txn);
-				
-				$Payment->appendChild($TxnList);
+			//Generate the XML
+			$SecurePayMessage = new XMLElement('SecurePayMessage');
 
-				$SecurePayMessage->appendChild($MessageInfo);
-				$SecurePayMessage->appendChild($MerchantInfo);
-				$SecurePayMessage->appendChild($RequestType);
-				$SecurePayMessage->appendChild($Payment);
-				
-				//Curl
-				require_once('library/curl.php');
-				$curl = new Curl;
-				$curl_result = $curl->post(
-					$this->getgatewayURI(),
-					$SecurePayMessage->generate()
-				);
-				
+			$MessageInfo = new XMLElement('MessageInfo');
+			$MessageInfo->appendChild(new XMLElement('messageID',$request_array['messageID']));
+			$MessageInfo->appendChild(new XMLElement('messageTimestamp',$request_array['messageTimestamp']));
+			$MessageInfo->appendChild(new XMLElement('timeoutValue',$request_array['timeoutValue']));
+			$MessageInfo->appendChild(new XMLElement('apiVersion',$request_array['apiVersion']));
+			$SecurePayMessage->appendChild($MessageInfo);
+
+			$MerchantInfo = new XMLElement('MerchantInfo');
+			$MerchantInfo->appendChild(new XMLElement('merchantID',$request_array['merchantID']));
+			$MerchantInfo->appendChild(new XMLElement('password',$request_array['password']));
+			$SecurePayMessage->appendChild($MerchantInfo);
+
+			$RequestType = new XMLElement('RequestType',$request_array['RequestType']);
+			$SecurePayMessage->appendChild($RequestType);
+
+			$Txn = new XMLElement('Txn');
+			$Txn->setAttribute('ID', '1');
+			$Txn->appendChild(new XMLElement('txnType',$request_array['txnType']));
+			$Txn->appendChild(new XMLElement('txnSource',$request_array['txnSource']));
+			$Txn->appendChild(new XMLElement('amount',$request_array['amount']));
+			$Txn->appendChild(new XMLElement('purchaseOrderNo',$request_array['purchaseOrderNo']));
+
+			$CreditCardInfo = new XMLElement('CreditCardInfo');
+			$CreditCardInfo->appendChild(new XMLElement('cardNumber',$request_array['cardNumber']));
+			$CreditCardInfo->appendChild(new XMLElement('expiryDate',$request_array['expiryDate']));
+			$CreditCardInfo->appendChild(new XMLElement('cvv', $request_array['cvv']));
+
+			$Txn->appendChild($CreditCardInfo);
+
+			$TxnList = new XMLElement('TxnList');
+			$TxnList->setAttribute('count','1');
+			$TxnList->appendChild($Txn);
+
+			$Payment = new XMLElement('Payment');
+			$Payment->appendChild($TxnList);
+			$SecurePayMessage->appendChild($Payment);
+
+			// Curl
+			require_once(TOOLKIT . '/class.gateway.php');
+			$curl = new Gateway;
+			$curl->init(self::getGatewayURI());
+			$curl->setopt('POST', true);
+			$curl->setopt('POSTFIELDS', $SecurePayMessage->asXML());
+
+			$curl_result = $curl->exec();
+			$info = $curl->getInfoLast();
+
+			// The Gateway did not connect to eWay successfully
+			if(!in_array($info["http_code"], array('200', '201'))) {
+				Symphony::$Log->pushToLog($error, E_USER_ERROR, true);
+
+				// Return a `GATEWAY_ERROR`
+				return(array(
+					'status' => __('Gateway error'),
+					'response-code' => Extension_SecurePay::GATEWAY_ERROR,
+					'response-message' => __('There was an error connecting to SecurePay.')
+				));
+			}
+
+			else {
 				// Create a document for the result and load the resul
 				$securepay_result = new DOMDocument('1.0', 'utf-8');
 				$securepay_result->formatOutput = true;
-				$securepay_result->loadXML($curl_result->body);
+				$securepay_result->loadXML($curl_result);
 				$securepay_result_xpath = new DOMXPath($securepay_result);
 
 				// Generate status result:
-				$securepay_transaction_id   = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/txnID)');
+				$securepay_transaction_id = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/txnID)');
 
-				if (strtolower($securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/approved)')) == 'yes') {
-					$securepay_approved = true;
-				} else {
-					$securepay_approved = false;
-				}
+				$securepay_code = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/responseCode)');;
+				$securepay_message = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/responseText)');
 
-				$securepay_error_code = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/responseCode)');;
-				$securepay_error_message = $securepay_result_xpath->evaluate('string(/SecurePayMessage/Payment/TxnList/Txn/responseText)');
-				
 				return(array(
-					'pgi-transaction-id'=> $securepay_transaction_id,
-					'status'		=> ($securepay_approved ? 'Approved' : 'Declined'),
-					'response-code'	=> $securepay_error_code,
-					'response-message'	=> $securepay_error_message
-				));
-			} else {
-				return(array(
-					'status'		=> 'Error',
-					'response-code'	=> $securepay_error_code,
-					'response-message'	=> 'Missing Fields: '.implode(', ', $missing_fields),
-					'missing-fields'=>	$missing_fields
+					'status' => in_array($eway_code, Extension_SecurePay::$approved_codes) ? __('Approved') : __('Declined'),
+					'response-code' => $securepay_code,
+					'response-message' => $securepay_message,
+					'pgi-transaction-id' => $securepay_transaction_id,
 				));
 			}
 		}
 	}
-
-?>
